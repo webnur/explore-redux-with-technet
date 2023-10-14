94d5c5691d8c29f991c33b5deeaeb52cf7d6ce0b
Nurul Islam <56204522+webnur@users.noreply.github.com>
2023-10-14 19:03:02 +0600

    edit some page

--- src/components/ProductReview.tsx
+++ src/components/ProductReview.tsx
@@ -3,13 +3,14 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
 import { FiSend } from 'react-icons/fi';
+import { useGetCommentQuery, usePostCommentMutation } from '@/redux/api/apiSlice';
 
-const dummyComments = [
-  'Bhalo na',
-  'Ki shob ghori egula??',
-  'Eta kono product holo ??',
-  '200 taka dibo, hobe ??',
-];
+// const data?.comment = [
+//   'Bhalo na',
+//   'Ki shob ghori egula??',
+//   'Eta kono product holo ??',
+//   '200 taka dibo, hobe ??',
+// ];
 
 interface IProps {
   id: string;
@@ -17,10 +18,22 @@ interface IProps {
 
 export default function ProductReview({ id }: IProps) {
   const [inputValue, setInputValue] = useState<string>('');
-  console.log(inputValue);
+
+  const [postComment, {isLoading, isError, isSuccess}] = usePostCommentMutation()
+const {data} = useGetCommentQuery(id,{refetchOnMountOrArgChange: true, pollingInterval: 30000})
+  console.log(isLoading);
+  console.log(isError);
+  console.log(isSuccess);
+
+
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    const options = {
+      id: id,
+      data: {comment: inputValue}
+    }
+    postComment(options)
     setInputValue('');
   };
 
@@ -44,7 +57,7 @@ export default function ProductReview({ id }: IProps) {
         </Button>
       </form>
       <div className="mt-10">
-        {dummyComments.map((comment, index) => (
+        {data?.comments.map((comment: string, index: number) => (
           <div key={index} className="flex gap-3 items-center mb-5">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -1,16 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// export const api = createApi({
+//   reducerPath: 'api',
+//   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
+//   tagTypes: ['comments'],
+//   endpoints: (builder) => ({
+//     getProducts: builder.query({
+//       query: () => '/products',
+//     }),
+//     singleProduct: builder.query({
+//       query: (id) => `/product/${id}`,
+//     }),
+
+//     postComment: builder.mutation({
+//       query: ({ id, data }) => ({
+//         url: `/comment/${id}`,
+//         method: 'POST',
+//         body: data,
+//       }),
+//       invalidatesTags: ['comments'],
+//     }),
+
+//     getComment: builder.query({
+//       query: (id) => `/comment/${id}`,
+//       providesTags: ['comments'],
+//     }),
+//   }),
+// });
+
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
-  endpoints: (builder) => ({
-    getProducts: builder.query({
-      query: () => '/products',
-    }),
-    singleProduct: builder.query({
-      query: (id) => `/product/${id}`,
-    }),
-  }),
+  tagTypes: ['comments'],
+  endpoints: () => ({}),
 });
-
-export const { useGetProductsQuery, useSingleProductQuery } = api;
